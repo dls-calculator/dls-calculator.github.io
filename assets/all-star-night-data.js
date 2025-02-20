a08d5027a78991e97f496805437c6b5c9808b576
sundrycode <sundrycode@users.noreply.github.com>
2025-02-20 04:34:05 +0000

    deploy: 5555a45a9b2f4a6dff140535aebd6f4456bb6c26

---
+++ assets/all-star-night-data.js
@@ -0,0 +1,300 @@
+
+const data = {
+    "phase-1": {
+      "phaseTotal": 0,
+      "items": [
+        {
+          "name": "Construction: 1 Minute",
+          "quantity": 0,
+          "rate": "250 per min.",
+          "formula": 250,
+          "points": 0
+        },
+        {
+          "name": "Construction: 5 Minute",
+          "quantity": 0,
+          "rate": "250 per min.",
+          "formula": (250*5),
+          "points": 0
+        },
+        {
+          "name": "Construction: 15 Minute",
+          "quantity": 0,
+          "rate": "250 per min.",
+          "formula": (250*15),
+          "points": 0
+        },
+        {
+          "name": "Construction: 30 Minute",
+          "quantity": 0,
+          "rate": "250 per min.",
+          "formula": (250*30),
+          "points": 0
+        },
+        {
+          "name": "Construction: 60 Minute",
+          "quantity": 0,
+          "rate": "250 per min.",
+          "formula": (250*60),
+          "points": 0
+        },
+        {
+          "name": "Construction: 4 Hour",
+          "quantity": 0,
+          "rate": "250 per min.",
+          "formula": (250*(4*60)),
+          "points": 0
+        },
+        {
+          "name": "Construction: 8 Hour",
+          "quantity": 0,
+          "rate": "250 per min.",
+          "formula": (250*(8*60)),
+          "points": 0
+        },
+        {
+          "name": "Construction: 12 Hour",
+          "quantity": 0,
+          "rate": "250 per min.",
+          "formula": (250*(12*60)),
+          "points": 0
+        },
+        {
+          "name": "Construction: 15 Hour",
+          "quantity": 0,
+          "rate": "250 per min.",
+          "formula": (250*(15*60)),
+          "points": 0
+        },
+        {
+          "name": "Construction: 1 Day",
+          "quantity": 0,
+          "rate": "250 per min.",
+          "formula": (250*(24*60)),
+          "points": 0
+        },
+  
+        {
+          "name": "Research: 1 Minute",
+          "quantity": 0,
+          "rate": "250 per min.",
+          "formula": 250,
+          "points": 0
+        },
+        {
+          "name": "Research: 5 Minute",
+          "quantity": 0,
+          "rate": "250 per min.",
+          "formula": (250*5),
+          "points": 0
+        },
+        {
+          "name": "Research: 15 Minute",
+          "quantity": 0,
+          "rate": "250 per min.",
+          "formula": (250*15),
+          "points": 0
+        },
+        {
+          "name": "Research: 30 Minute",
+          "quantity": 0,
+          "rate": "250 per min.",
+          "formula": (250*30),
+          "points": 0
+        },
+        {
+          "name": "Research: 60 Minute",
+          "quantity": 0,
+          "rate": "250 per min.",
+          "formula": (250*60),
+          "points": 0
+        },
+        {
+          "name": "Research: 4 Hour",
+          "quantity": 0,
+          "rate": "250 per min.",
+          "formula": (250*(4*60)),
+          "points": 0
+        },
+        {
+          "name": "Research: 8 Hour",
+          "quantity": 0,
+          "rate": "250 per min.",
+          "formula": (250*(8*60)),
+          "points": 0
+        },
+        {
+          "name": "Research: 12 Hour",
+          "quantity": 0,
+          "rate": "250 per min.",
+          "formula": (250*(12*60)),
+          "points": 0
+        },
+        {
+          "name": "Research: 15 Hour",
+          "quantity": 0,
+          "rate": "250 per min.",
+          "formula": (250*(15*60)),
+          "points": 0
+        },
+        {
+          "name": "Research: 1 Day",
+          "quantity": 0,
+          "rate": "250 per min.",
+          "formula": (250*(24*60)),
+          "points": 0
+        },
+  
+        {
+          "name": "Training: 1 Minute",
+          "quantity": 0,
+          "rate": "250 per min.",
+          "formula": 250,
+          "points": 0
+        },
+        {
+          "name": "Training: 5 Minute",
+          "quantity": 0,
+          "rate": "250 per min.",
+          "formula": (250*5),
+          "points": 0
+        },
+        {
+          "name": "Training: 15 Minute",
+          "quantity": 0,
+          "rate": "250 per min.",
+          "formula": (250*15),
+          "points": 0
+        },
+        {
+          "name": "Training: 30 Minute",
+          "quantity": 0,
+          "rate": "250 per min.",
+          "formula": (250*30),
+          "points": 0
+        },
+        {
+          "name": "Training: 60 Minute",
+          "quantity": 0,
+          "rate": "250 per min.",
+          "formula": (250*60),
+          "points": 0
+        },
+        {
+          "name": "Training: 4 Hour",
+          "quantity": 0,
+          "rate": "250 per min.",
+          "formula": (250*(4*60)),
+          "points": 0
+        },
+        {
+          "name": "Training: 8 Hour",
+          "quantity": 0,
+          "rate": "250 per min.",
+          "formula": (250*(8*60)),
+          "points": 0
+        },
+        {
+          "name": "Training: 12 Hour",
+          "quantity": 0,
+          "rate": "250 per min.",
+          "formula": (250*(12*60)),
+          "points": 0
+        },
+        {
+          "name": "Training: 15 Hour",
+          "quantity": 0,
+          "rate": "250 per min.",
+          "formula": (250*(15*60)),
+          "points": 0
+        },
+        {
+          "name": "Training: 1 Day",
+          "quantity": 0,
+          "rate": "250 per min.",
+          "formula": (250*(24*60)),
+          "points": 0
+        },
+  
+        {
+          "name": "Universal: 1 Minute",
+          "quantity": 0,
+          "rate": "250 per min.",
+          "formula": 250,
+          "points": 0
+        },
+        {
+          "name": "Universal: 5 Minute",
+          "quantity": 0,
+          "rate": "250 per min.",
+          "formula": (250*5),
+          "points": 0
+        },
+        {
+          "name": "Universal: 15 Minute",
+          "quantity": 0,
+          "rate": "250 per min.",
+          "formula": (250*15),
+          "points": 0
+        },
+        {
+          "name": "Universal: 30 Minute",
+          "quantity": 0,
+          "rate": "250 per min.",
+          "formula": (250*30),
+          "points": 0
+        },
+        {
+          "name": "Universal: 60 Minute",
+          "quantity": 0,
+          "rate": "250 per min.",
+          "formula": (250*60),
+          "points": 0
+        },
+        {
+          "name": "Universal: 4 Hour",
+          "quantity": 0,
+          "rate": "250 per min.",
+          "formula": (250*(4*60)),
+          "points": 0
+        },
+        {
+          "name": "Universal: 8 Hour",
+          "quantity": 0,
+          "rate": "250 per min.",
+          "formula": (250*(8*60)),
+          "points": 0
+        },
+        {
+          "name": "Universal: 12 Hour",
+          "quantity": 0,
+          "rate": "250 per min.",
+          "formula": (250*(12*60)),
+          "points": 0
+        },
+        {
+          "name": "Universal: 15 Hour",
+          "quantity": 0,
+          "rate": "250 per min.",
+          "formula": (250*(15*60)),
+          "points": 0
+        },
+        {
+          "name": "Universal: 1 Day",
+          "quantity": 0,
+          "rate": "250 per min.",
+          "formula": (250*(24*60)),
+          "points": 0
+        },
+        {
+            "name": "Exquisite File",
+            "quantity": 0,
+            "rate": "10,800 each",
+            "formula": 10800,
+            "points": 0
+          },
+      ], 
+    }
+    
+  
+  };
